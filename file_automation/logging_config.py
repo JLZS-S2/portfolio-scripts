5e70fcb44b05525d6d67d2a5b5bdbd3a9545dae6
JLZS-S2 <joaolzss604@gmail.com>
2026-06-03 15:46:53 -0300

    add file_automation project with README

---
+++ file_automation/logging_config.py
@@ -0,0 +1,9 @@
+import logging
+import logging.config
+import json
+
+def setup_logging(config_file="logging_config.json", logger_name="File Organizer Pipeline"):
+    with open(config_file, "r") as f:
+        config = json.load(f)
+    logging.config.dictConfig(config)
+    return logging.getLogger(logger_name)

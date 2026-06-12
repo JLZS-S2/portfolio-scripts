7ebe690e3a8803f4d564a5a0f7692a12632d5406
JLZS-S2 <joaolzss604@gmail.com>
2026-06-12 15:03:43 -0300

    remove weather_pipeline folder from tracking

--- weather_pipeline/logging_config.py
+++
@@ -1,9 +0,0 @@
-import logging
-import logging.config
-import json
-
-def setup_logging(config_file="logging_config.json", logger_name="Weather Data Pipeline"):
-    with open(config_file, "r") as f:
-        config = json.load(f)
-    logging.config.dictConfig(config)
-    return logging.getLogger(logger_name)
